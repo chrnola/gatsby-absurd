85620b96f9258a5ebdf211b34720bdecaa8d77cb
Chris Pinola <chris@pinola.co>
2020-11-02 22:48:32 -0500

    Living on the edge

--- src/components/sections/Schedule.js
+++ src/components/sections/Schedule.js
@@ -1,15 +1,20 @@
 import React from 'react';
-import { Helmet } from 'react-helmet';
+import { Section, Container } from '@components/global';
+import InnerHTML from 'dangerously-set-html-content';
 
 const Schedule = () => (
-    <Helmet>
-        <script
-            id="nc-booking-widget-script"
-            src="https://www.neatcal.com/app/services/booking/assets/js/integrate.js"
-            data-access-key="ccd6b05960ab7a388b98eec40613b86c"
-            data-nc-root="https://www.neatcal.com/app"
-            data-booking-url="https://www.neatcal.com/app/services/booking/book?u=ccd6b05960ab7a388b98eec40613b86c&widget_order=categories,resources,date&photo_mode=0&click_and_go=1&class_list_tmpl=list-1"></script>
-    </Helmet>
+    <Section id="schedule">
+        <Container>
+            <h1>Book an Appointment</h1>
+            <InnerHTML html={`<script
+                id="nc-booking-widget-script"
+                src="https://www.neatcal.com/app/services/booking/assets/js/integrate.js"
+                data-access-key="ccd6b05960ab7a388b98eec40613b86c"
+                data-nc-root="https://www.neatcal.com/app"
+                data-booking-url="https://www.neatcal.com/app/services/booking/book?u=ccd6b05960ab7a388b98eec40613b86c&widget_order=categories,resources,date&photo_mode=0&click_and_go=1&class_list_tmpl=list-1">
+            </script>`}/>
+        </Container>
+    </Section>
 );
 
 export default Schedule;
