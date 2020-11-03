3252b150b1629776c03f82fdbd3aca1f65c30f6e
Chris Pinola <chris@pinola.co>
2020-11-02 21:36:37 -0500

    Revert "Update About.js"
    This reverts commit ade3d893f604607e57ffa2358457b2a4eb1ef9fb.

--- src/components/sections/About.js
+++ src/components/sections/About.js
@@ -45,12 +45,6 @@ const About = () => (
     `}
     render={data => (
       <Section id="about">
-        <script
-          id="nc-booking-widget-script"
-          src="https://www.neatcal.com/app/services/booking/assets/js/integrate.js"
-          data-access-key="ccd6b05960ab7a388b98eec40613b86c"
-          data-nc-root="https://www.neatcal.com/app"
-          data-booking-url="https://www.neatcal.com/app/services/booking/book?u=ccd6b05960ab7a388b98eec40613b86c&widget_order=categories,resources,date&photo_mode=0&click_and_go=1&class_list_tmpl=list-1"></script>
         <Container>
           <Grid>
             <div>
