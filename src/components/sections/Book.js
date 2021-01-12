7135c953805e4369952e894e6b2e04001e628284
Chris Pinola <chris@pinola.co>
2021-01-11 22:15:08 -0500

    Update widget order

--- src/components/sections/Book.js
+++ src/components/sections/Book.js
@@ -11,7 +11,7 @@ const Book = () => (
                 src="https://www.neatcal.com/app/services/booking/assets/js/integrate.js"
                 data-access-key="ccd6b05960ab7a388b98eec40613b86c"
                 data-nc-root="https://www.neatcal.com/app"
-                data-booking-url="https://www.neatcal.com/app/services/booking/book?u=ccd6b05960ab7a388b98eec40613b86c&widget_order=categories,resources,date&photo_mode=0&click_and_go=1&class_list_tmpl=list-1">
+                data-booking-url="https://www.neatcal.com/app/services/booking/book?u=ccd6b05960ab7a388b98eec40613b86c&widget_order=categories,all_services,staff,time&photo_mode=0&click_and_go=1&class_list_tmpl=list-1">
             </script>`}/>
         </Container>
     </Section>
